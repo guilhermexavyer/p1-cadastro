ceb7df1a0f07a26c2f396398c932a126c134b0b1
guilhermexavyer <guilhermexavierpessoal@gmail.com>
2024-10-26 19:27:57 -0300

    Enviando no e-mail

--- src/TelaCliente.java
+++ src/TelaCliente.java
@@ -1,3 +1,11 @@
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -5,6 +13,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import java.util.Properties;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedWriter;
@@ -25,52 +34,64 @@ public class TelaCliente {
         panel.setLayout(null);
 
         JLabel lblNome = new JLabel("Nome");
-        lblNome.setBounds(10, 10, 150, 25);
+        lblNome.setBounds(10, 10, 200, 25);
         panel.add(lblNome);
 
         JTextField txtNome = new JTextField();
-        txtNome.setBounds(10, 30, 150, 25);
+        txtNome.setBounds(10, 30, 200, 25);
         panel.add(txtNome);
 
         JLabel lblCPF = new JLabel("CPF");
-        lblCPF.setBounds(10, 60, 150, 25);
+        lblCPF.setBounds(10, 60, 200, 25);
         panel.add(lblCPF);
 
         JTextField txtCPF = new JTextField();
-        txtCPF.setBounds(10, 80, 150, 25);
+        txtCPF.setBounds(10, 80, 200, 25);
         panel.add(txtCPF);
 
         JLabel lblRG = new JLabel("RG");
-        lblRG.setBounds(10, 110, 150, 25);
+        lblRG.setBounds(10, 110, 200, 25);
         panel.add(lblRG);
 
         JTextField txtRG = new JTextField();
-        txtRG.setBounds(10, 130, 150, 25);
+        txtRG.setBounds(10, 130, 200, 25);
         panel.add(txtRG);
 
         JLabel lblEndereco = new JLabel("Endereço");
-        lblEndereco.setBounds(10, 160, 150, 25);
+        lblEndereco.setBounds(10, 160, 200, 25);
         panel.add(lblEndereco);
 
         JTextField txtEndereco = new JTextField();
-        txtEndereco.setBounds(10, 180, 150, 25);
+        txtEndereco.setBounds(10, 180, 200, 25);
         panel.add(txtEndereco);
 
         JLabel lblTelefone = new JLabel("Telefone");
-        lblTelefone.setBounds(10, 210, 150, 25);
+        lblTelefone.setBounds(10, 210, 200, 25);
         panel.add(lblTelefone);
 
         JTextField txtTelefone = new JTextField();
-        txtTelefone.setBounds(10, 230, 150, 25);
+        txtTelefone.setBounds(10, 230, 200, 25);
         panel.add(txtTelefone);
 
+        JLabel lblEmailDestino = new JLabel("Email de destino");
+        lblEmailDestino.setBounds(10, 260, 200, 25);
+        panel.add(lblEmailDestino);
+
+        JTextField txtEmailDestino = new JTextField();
+        txtEmailDestino.setBounds(10, 280, 200, 25);
+        panel.add(txtEmailDestino);
+
         JButton btnSalvar = new JButton("Salvar");
-        btnSalvar.setBounds(10, 270, 150, 25);
+        btnSalvar.setBounds(10, 310, 200, 25);
         panel.add(btnSalvar);
 
         JButton btnExcluir = new JButton("Excluir");
-        btnExcluir.setBounds(10, 300, 150, 25);
+        btnExcluir.setBounds(10, 340, 200, 25);
         panel.add(btnExcluir);
+
+        JButton btnEnviarEmail = new JButton("Enviar no Email");
+        btnEnviarEmail.setBounds(10, 370, 200, 25);
+        panel.add(btnEnviarEmail);
 
         btnSalvar.addActionListener(new ActionListener() {
             @Override
@@ -92,11 +113,18 @@ public class TelaCliente {
             }
         });
 
+        btnEnviarEmail.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                enviarEmail(txtEmailDestino.getText());
+            }
+        });
+
         tela.setVisible(true);
     }
 
     private static void geraArquivo(Cliente cliente) {
-        String caminhoArquivo = "p1.txt";
+        String caminhoArquivo = "p1-cadastro.txt";
 
         File arquivo = new File(caminhoArquivo);
 
@@ -122,7 +150,7 @@ public class TelaCliente {
     }
 
     private static void excluiArquivo() {
-        String caminhoArquivo = "p1.txt";
+        String caminhoArquivo = "p1-cadastro.txt";
 
         File arquivo = new File(caminhoArquivo);
 
@@ -136,6 +164,61 @@ public class TelaCliente {
             "Erro ao excluir arquivo",
             "Erro",
             1);
+        }
+    }
+
+    private static void enviarEmail(String emailDestino) {
+        // Configurações do servidor SMTP
+        String host = "smtp.gmail.com";
+        final String user = "guilhermexavierpessoal@gmail.com";
+        final String password = "Topeira.01";
+
+        String para = emailDestino;
+
+        // Propriedades do servidor SMTP
+        Properties proprieties = new Properties();
+        proprieties.put("mail.smtp.host", host);
+        proprieties.put("mail.smtp.host", true);
+        proprieties.put("mail.smtp.host", 587); // 587 ou 465
+        proprieties.put("mail.smtp.starttls.host", true); // Requer STARTTLS
+
+        // Criação de uma sessão com autenticação
+        Session session = null;
+        try {
+            session = Session.getInstance(proprieties,
+            new javax.mail.Authenticator() {
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(user, password);
+                }
+            });
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null,
+            "Erro ao conectar ao gmail." + e.getMessage(),
+            "Erro",
+            0);
+        }
+
+        try {
+            // Criação da mensagem
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(user));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(para));
+            message.setSubject("Teste de envio de e-mail na disciplina de Programação I");
+            message.setText("Estou enviando um email de teste.");
+
+            // Envio do e-mail
+            Transport.send(message);
+
+            JOptionPane.showMessageDialog(null,
+            "Email enviado",
+            "E-mail",
+            0);
+
+        } catch (MessagingException e) {
+            JOptionPane.showMessageDialog(null,
+            "Erro ao enviar e-mail. " + e.getMessage(),
+            "Erro",
+            0);
         }
     }
 }
